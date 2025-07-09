c6756960375d707c496b01447f56bb386252b64c
Rizvan Adzhoyan <100145184+radzhoyan@users.noreply.github.com>
2025-07-09 10:20:26 +0600

    update gradle version and libs

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -152,7 +152,8 @@ class _MyAppState extends State<MyApp> {
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: <Widget>[
                   ElevatedButton(
-                    style: ElevatedButton.styleFrom(primary: Colors.brown),
+                    style:
+                        ElevatedButton.styleFrom(backgroundColor: Colors.brown),
                     onPressed: () {
                       initPlatformState();
                     },
@@ -164,7 +165,8 @@ class _MyAppState extends State<MyApp> {
                   const SizedBox(width: 20),
                   ElevatedButton(
                     style: ElevatedButton.styleFrom(
-                        primary: _connected ? Colors.red : Colors.green),
+                        backgroundColor:
+                            _connected ? Colors.red : Colors.green),
                     onPressed: _connected ? _disconnect : _connect,
                     child: Text(
                       _connected ? 'Disconnect' : 'Connect',
@@ -177,7 +179,8 @@ class _MyAppState extends State<MyApp> {
                 padding:
                     const EdgeInsets.only(left: 10.0, right: 10.0, top: 50),
                 child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(primary: Colors.brown),
+                  style:
+                      ElevatedButton.styleFrom(backgroundColor: Colors.brown),
                   onPressed: () {
                     testPrint.sample();
                   },
